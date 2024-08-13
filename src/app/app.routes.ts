6f268c2e1140d998d3ee3d52ea6a16c405b05839
Ray030091 <137258932+Ray030091@users.noreply.github.com>
2024-08-13 23:48:00 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Component } from '@angular/core';
+export const routes: Routes = [
+ {
+   path: '',
+   component: FrontLayoutComponent,
+   children: [
+      {
+         path: '',
+   component: HomeComponent,
+      },
+      {
+         path: 'about',
+   component: AboutComponent,
+      },
+      {
+         path: 'login',
+   component: LoginComponent,
+      },
+      {
+         path: ' register',
+   component:  RegisterComponent,
+      },
+      {
+         path: ' Component',
+   component:  Component,
+      },
+   ]
+ }
 
-export const routes: Routes = [];
+];
